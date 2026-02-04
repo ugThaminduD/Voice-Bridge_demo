36c45dc71a6a23376715cff44790afa763010611
Dilhara U G T <it22309624@my.sliit.lk>
2026-02-04 22:56:57 +0530

    Update ApiKeyConfig.kt

--- app/src/main/java/com/chirathi/voicebridge/data/ApiKeyConfig.kt
+++ app/src/main/java/com/chirathi/voicebridge/data/ApiKeyConfig.kt
@@ -2,5 +2,5 @@ package com.chirathi.voicebridge.data
 
 object ApiKeyConfig {
     // Get a new API key from: https://makersuite.google.com/app/apikey
-    const val GEMINI_API_KEY = "AIzaSyAeXwjU8yrlM2ihjp9VsSyeA5CHhP4jBf0"
+    const val GEMINI_API_KEY = "jp9VsSyeA5CHhP4jBf0"
 }
